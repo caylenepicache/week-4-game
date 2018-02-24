c86bf649d4913be7e2e71449c0d04d7803528a03
unknown <caylenepicache@gmail.com>
2018-02-23 19:41:09 -0800

    changes to css/html and added images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,119 +1,110 @@
-// GLOBAL VARIABLES
+//global variables
 var wins = 0;
 var losses = 0;
-var score = 0;
 
-var targetNum = 0;
-
-var randomOneValue = Math.floor(Math.random() * 12) + 1 ;
-var randomTwoValue = Math.floor(Math.random() * 12) + 1 ;
-var randomThreeValue = Math.floor(Math.random() * 12) + 1 ;
-var randomFourValue = Math.floor(Math.random() * 12) + 1 ;
-
-var keepTrackScore = 0;
-
-//FUNCTIONS
-
-//randomizer number
-
-function variableReset() {
-
-var randomOneValue = Math.floor(Math.random() * 12) + 1 ;
-var randomTwoValue = Math.floor(Math.random() * 12) + 1 ;
-var randomThreeValue = Math.floor(Math.random() * 12) + 1 ;
-var randomFourValue = Math.floor(Math.random() * 12) + 1 ;
+var randomOneValue;
+var randomTwoValue;
+var randomThreeValue;
+var randomFourValue;
 
 var keepTrackScore = 0;
 var randomInt = 0;
 
-}
+//functions
 
+function variableReset() {
 
-function gameClicks() {
-
+    $( "#displayCrysOne" ).unbind( "click" );
+    $( "#displayCrysTwo" ).unbind( "click" );
+    $( "#displayCrysThree" ).unbind( "click" );
+    $( "#displayCrysFour" ).unbind( "click" ); 
   
-  var randomInt = Math.floor(Math.random() * 120) + 18;
-  document.getElementById("number-to-guess").innerHTML = randomInt;
-  console.log(randomInt);
+  
+    randomOneValue = Math.floor(Math.random() * 12) + 1 ;
+    randomTwoValue = Math.floor(Math.random() * 12) + 1 ;
+    randomThreeValue = Math.floor(Math.random() * 12) + 1 ;
+    randomFourValue = Math.floor(Math.random() * 12) + 1 ;
+  
+    keepTrackScore = 0;
+    randomInt = 0;
+  
+    $("#userPoints").html(keepTrackScore);
+  
+  
+    gameClicks();
+  
+  };
 
-  $("#displayCrysOne").on("click", function() {
-  keepTrackScore = keepTrackScore + randomOneValue;
-  $("#userPoints").html(keepTrackScore);
-  checker();
-  console.log(randomOneValue);
+  function gameClicks() {
 
-  }) 
-
-  $("#displayCrysTwo").on("click", function() {
-  keepTrackScore = keepTrackScore + randomTwoValue;
-  $("#userPoints").html(keepTrackScore);
-  checker();
-  console.log(randomTwoValue);
-
-  }) 
-
-  $("#displayCrysThree").on("click", function() {
-  keepTrackScore = keepTrackScore + randomThreeValue;
-  $("#userPoints").html(keepTrackScore);
-  checker();
-  console.log(randomThreeValue);
-
-  }) 
-
-  $("#displayCrysFour").on("click", function() {
-  keepTrackScore = keepTrackScore + randomFourValue;
-  $("#userPoints").html(keepTrackScore);
-  checker();
-  console.log(randomFourValue);
-
-  }) 
-
-console.log(keepTrackScore);
-console.log(randomInt);
-
-function checker() {
-
-if (randomInt === keepTrackScore) {
-  wins++;
-  console.log("user win");
-  variableReset();
-  gameClicks();
-}
-else if (keepTrackScore > randomInt) {
-  losses++;
-  console.log("user lost");
-  variableReset();
-  gameClicks();
-}
-};
-
-};
-/*
-function checker() {
-
-  if (randomInt === keepTrackScore) {
-    wins++;
-    console.log("user win");
-    variableReset();
+    var randomInt = Math.floor(Math.random() * 120) + 18;
+    document.getElementById("number-to-guess").innerHTML = randomInt;
+    console.log("random: "+randomInt);
+  
+  
+    $("#displayCrysOne").on("click", function() {
+    keepTrackScore = keepTrackScore + randomOneValue;
+    $("#userPoints").html(keepTrackScore);
+    checker();
+    console.log("Crystal one value: "+ randomOneValue);
+    console.log("keeptrackscore: " + keepTrackScore);
+    }) 
+  
+    $("#displayCrysTwo").on("click", function() {
+    keepTrackScore = keepTrackScore + randomTwoValue;
+    $("#userPoints").html(keepTrackScore);
+    checker();
+    console.log("Crystal two value: "+ randomTwoValue);
+    console.log("keeptrackscore: " + keepTrackScore);
+  
+    }) 
+  
+    $("#displayCrysThree").on("click", function() {
+    keepTrackScore = keepTrackScore + randomThreeValue;
+    $("#userPoints").html(keepTrackScore);
+    checker();
+    console.log("Crystal three value: "+ randomThreeValue);
+    console.log("keeptrackscore: " + keepTrackScore);
+  
+    }) 
+  
+    $("#displayCrysFour").on("click", function() {
+    keepTrackScore = keepTrackScore + randomFourValue;
+    $("#userPoints").html(keepTrackScore);
+    checker();
+    console.log("Crystal four value: "+ randomFourValue);
+    console.log("keeptrackscore: " + keepTrackScore);
+  
+    }) 
+  
+  
+  function checker() {
+  
+    if (randomInt === keepTrackScore) {
+      wins++;
+      console.log(wins);
+      console.log("user win");
+      $("#result").html("You win!");
+      variableReset();
+      keepTabsVariables();
+    }
+    if (keepTrackScore > randomInt) {
+      losses++;
+      console.log(losses);
+      console.log("user lost");
+      $("#result").html("You Lose!");
+      variableReset();
+      keepTabsVariables();
+    }
   }
-  else if (keepTrackScore > randomInt) {
-    losses++;
-    console.log("user lost");
-    variableReset();
-  }
-}; */
+  
+  
+  };
+  
+  function keepTabsVariables () {
+    $("#winScore").html(wins);
+    $("#lossScore").html(losses);
+  };
 
-
-
-function keepTabsVariables () {
-  $("#winScore").html(wins);
-  $("#lossScore").html(losses);
-  $("#userPoints").html(keepTrackScore);
-}
-
-
-
-//main process
 variableReset();
-gameClicks();
 keepTabsVariables();
